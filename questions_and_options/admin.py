6c86b7d7ba0b25e66a63f054f62372a6c89fd027
HelloSolo <davidalimi148@gmail.com>
2022-07-07 09:15:42 +0100

    added admin page

--- questions_and_options/admin.py
+++ questions_and_options/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from django.db.models import Q
+from . import models
 
 # Register your models here.
+
+
+class OptionInline(admin.TabularInline):
+    model = models.Option
+
+
+@admin.register(models.Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = [
+        'description',
+        'image',
+        'options',
+        'answer'
+    ]
+    inlines = [OptionInline]
+
+    def options(self, question):
+        queryset = list(models.Option.objects.filter(question_id=question.id))
+        return queryset
+
+    def answer(self, question):
+        return list(models.Option.objects.filter(Q(question_id=question.id) & Q(answer=1)))
+
+
+@ admin.register(models.Option)
+class OptionAdmin(admin.ModelAdmin):
+    list_display = [
+        'question',
+        'description',
+        'answer',
+    ]
--- questions_and_options/models.py
+++ questions_and_options/models.py
@@ -1,19 +1,21 @@
-from email.mime import image
 from django.db import models
 
 # Create your models here.
 
 
-class Questions(models.Model):
-    description = models.CharField(max_length=100000000000)
-    image = models.ImageField(upload_to='questions/images')
+class Question(models.Model):
+    description = models.TextField()
+    image = models.ImageField(upload_to='questions/images', blank="True")
 
 
-class Options(models.Model):
+class Option(models.Model):
     question = models.ForeignKey(
-        Questions,
+        Question,
         on_delete=models.CASCADE,
         related_name='question',
     )
-    description = models.CharField(max_length=1000000000)
+    description = models.CharField(max_length=1000)
     answer = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return self.description
